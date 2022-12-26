0ffc54ed2f3820a45268f6f130ab6846bbe9587e
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-12-26 10:47:06 +0800

    Add files via upload

---
+++ days76_pilihan.py
@@ -0,0 +1,30 @@
+print (" pilih salah satu huruf dari a sampai d")
+while True:
+    i = input("plihan :")
+
+    a = "uang tunai"
+    b = "iphone"
+    c = "labtop"
+    d = " buku"
+
+    if i == "a":
+        print("anda mendapatkan",a)
+
+    elif i == "b":
+        print("anda mendapatkan",b)
+        
+    elif i == "c":
+        print("anda mendapatkan",c)
+    elif i == "d":
+        print("anda mendapatkan",b)
+
+    l = input("masih mau lanjut yes/no  :")
+    if l == "no":
+        break
+        
+
+
+
+
+
+
